90a25d4d9f49e2a0e1c5772420badda57c7e92eb
Susie Douang <soutsada.d@gmail.com>
2024-12-26 21:24:55 -0600

    add an about me section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,55 @@ async function Home() {
         <source src="downtown.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
+      <div className="bg-secondary h-[200px] content-center justify-items-center text-white">
+        <h2 className="text-2xl italic font-bold">Driving Cloud Projects Forward with Excellence and Strategy</h2>
+      </div>
+      {/* Intro */}
+      <div className="bg-white h-[400px]">
+        <div>
+          <h3>Hey, I'm Matt.</h3>
+          <h4>I help</h4>
+        </div>
+      </div>
+      {/* Banner */}
+      <div className="bg-secondary h-[200px] w-full content-center justify-items-center text-black">
+        {/* First */}
+        <div>
+          <div>
+            Icon 1
+          </div>
+          <div>
+            Title
+          </div>
+          <div>
+            SubTitle
+          </div>
+        </div>
+        {/* Second */}
+        <div>
+          <div>
+            Icon 2
+          </div>
+          <div>
+            Title
+          </div>
+          <div>
+            SubTitle
+          </div>
+        </div>
+        {/* Third */}
+        <div>
+          <div>
+            Icon 3
+          </div>
+          <div>
+            Title
+          </div>
+          <div>
+            SubTitle
+          </div>
+        </div>
+      </div>
     </ div>
   );
 }
