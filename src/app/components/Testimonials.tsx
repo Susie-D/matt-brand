552e2ff58fd296c71d9317400a6752f9d2dce0b6
Susie Douang <soutsada.d@gmail.com>
2025-03-01 21:38:21 -0600

    update testimonials for responsiveness

--- src/app/components/Testimonials.tsx
+++ src/app/components/Testimonials.tsx
@@ -1,30 +1,36 @@
-
 export default function Testimonials() {
     return (
-        <section className="bg-secondary p-14">
-            <h2 className="flex justify-center text-white font-semibold text-4xl italic text-center pb-10">What Clients Are Saying...</h2>
-            <div className="grid grid-row-1 grid-cols-4 gap-4">
-                <div className='content-around'>
-                    <p className='italic mb-3'> “Matt was instrumental in our assessment of thousands of applications in our global portfolio, a key integrator between our business objectives and technology capabilities.”</p>
-                    <p className='m-0 text-xs'>
-                        Ayman Nasser, Cargill
+        <section className="bg-gradient-to-r from-secondary to-primary/60 p-14">
+            <h2 id="testimonials" className="flex justify-center text-white font-semibold text-4xl italic text-center pb-10">
+                What Clients Are Saying...
+            </h2>
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+                <div className="content-around text-center sm:text-left">
+                    <p className="italic mb-3 text-lg  text-white font-bold">
+                        “Matt was instrumental in our assessment of thousands of applications in our global portfolio, a key integrator between our business objectives and technology capabilities.”
                     </p>
-                    <div className="m-0 text-xs font-bold">
-                        Global IT Modernization Lead
-                    </div>
+                    <p className="m-0 text-s">Ayman Nasser, Cargill</p>
+                    <div className="m-0 text-s font-bold">Global IT Modernization Lead</div>
                 </div>
-                <img src="/laptop.svg" alt="Public Folder Image" width="100%" />
-                <div className='content-around'>
-                    <p className='italic mb-3'> “We really appreciated the detailed migration planning, which ecompassed chattiness between servers at an individual level and tagging to appropriately track our FinOps.”</p>
-                    <p className='m-0 text-xs'>
-                        Aaron Murray, Securian Financial Enterprise
+                <img
+                    src="/laptop.svg"
+                    alt="Public Folder Image"
+                    className="w-full h-auto sm:w-3/4 lg:w-full mx-auto max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg"
+
+                />
+                <div className="content-around text-center sm:text-left">
+                    <p className="italic mb-3 text-lg text-white font-bold">
+                        “We really appreciated the detailed migration planning, which encompassed chattiness between servers at an individual level and tagging to appropriately track our FinOps.”
                     </p>
-                    <div className="m-0 text-xs font-bold">
-                        Cloud Director
-                    </div>
+                    <p className="m-0 text-s">Aaron Murray, Securian Financial Enterprise</p>
+                    <div className="m-0 text-s font-bold">Cloud Director</div>
                 </div>
-                <img src="/street.svg" alt="Public Folder Image" width="100%" />
+                <img
+                    src="/street.svg"
+                    alt="Public Folder Image"
+                    className="w-full h-auto sm:w-3/4 lg:w-full mx-auto max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg"
+                />
             </div>
-        </section >
-    )
+        </section>
+    );
 }
